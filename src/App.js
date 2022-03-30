c7cbc6081d30f0210c053f5eb2e5eee4423f1c5b
Juan Lucero <juaneslucero@gmail.com>
2022-03-29 22:09:14 -0700

    Remove CRA bloat and add basic coming soon page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import atreides from './assets/atreides.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={atreides} className="App-logo" alt="logo" />
+        <div>THE SPICE MUST FLOW</div>
+        <div className='App-link'>Coming Soon</div>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders iconic line', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const pElement = screen.getByText(/THE SPICE MUST FLOW/i);
+  expect(pElement).toBeInTheDocument();
 });
