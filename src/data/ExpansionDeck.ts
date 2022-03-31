03b0e89b4a474b167c4b3ea20dcb9e215b1f2ce3
Juan Lucero <juaneslucero@gmail.com>
2022-03-30 22:30:52 -0700

    Add base and expansion decks

---
+++ src/data/ExpansionDeck.ts
@@ -0,0 +1,18 @@
+import { TreacheryCard } from './TreacheryCard';
+
+export const expansionDeck: TreacheryCard[] = [
+    { name: 'Artillery Strike', kind: 'Weapon', type: 'Artillery Strike' },
+    { name: 'Poison Tooth', kind: 'Weapon', type: 'Poison Tooth' },
+    { name: 'Weirding Way', kind: 'Weapon/Defence', type: 'Weirding Way' },
+    { name: 'Chemistry', kind: 'Weapon/Defence', type: 'Chemistry' },
+    { name: 'Poison Blade', kind: 'Weapon', type: 'Poison Blade' },
+    { name: 'Shield Snooper', kind: 'Defence', type: 'Shield Snooper' },
+    { name: 'Hunter Seeker', kind: 'Weapon', type: 'Projectile' },
+    { name: 'Basilia Weapon', kind: 'Weapon', type: 'Poison' },
+    { name: 'Kull Wahad', kind: 'Useless' },
+    { name: 'Harvester', kind: 'Special', type: 'Harvester' },
+    { name: 'Thumper', kind: 'Special', type: 'Thumper' },
+    { name: 'Amal', kind: 'Special', type: 'Amal' },
+    { name: 'shield_5', kind: 'Defence', type: 'Shield' },
+    { name: 'snooper_5', kind: 'Defence', type: 'Snooper' },
+];
