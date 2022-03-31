2c9875ee5658001f836db7b059105548f9e2ba4b
Juan Lucero <juaneslucero@gmail.com>
2022-03-30 20:27:48 -0700

    Add prettier config and run format script

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders iconic line', () => {
-  render(<App />);
-  const pElement = screen.getByText(/THE SPICE MUST FLOW/i);
-  expect(pElement).toBeInTheDocument();
+    render(<App />);
+    const pElement = screen.getByText(/THE SPICE MUST FLOW/i);
+    expect(pElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import atreides from './assets/atreides.svg';
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={atreides} className="App-logo" alt="logo" />
-        <div>THE SPICE MUST FLOW</div>
-        <div className='App-link'>Coming Soon</div>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={atreides} className="App-logo" alt="logo" />
+                <div>THE SPICE MUST FLOW</div>
+                <div className="App-link">Coming Soon</div>
+            </header>
+        </div>
+    );
+};
 
 export default App;
